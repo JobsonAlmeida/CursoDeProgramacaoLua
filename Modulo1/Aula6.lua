1422d185c9c4918f9346040727542feaa08b435c
Jobson Almeida <jobsonbox@gmail.com>
2024-10-03 12:31:12 -0300

    The lesson on math library has been completed

---
+++ Modulo1/Aula6.lua
@@ -0,0 +1,16 @@
+local num1 = 20
+local num2 = 5.5
+
+
+--[[print(math.ceil(5.9))
+print(math.floor(num2))
+print(math.fmod(10,4))
+print(math.fmod(8,4))
+print(math.modf(2.5))]]
+
+print(math.max(4,8,2,3,7,2))
+print(math.min(4,8,2,3,7,2))
+
+print(math.random(10,25))
+print(math.pi)
+print(math.huge)
