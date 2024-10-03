73ad431fa71b76fd03a1fa616042d6911691f649
Jobson Almeida <jobsonbox@gmail.com>
2024-10-03 14:36:08 -0300

    the first lesson on string treatment has been completed

--- Modulo1/Aula7.lua
+++ Modulo1/Aula7.lua
@@ -2,10 +2,39 @@
 -- local thing = io.read()
 -- print("Seu nome é", thing)
 
-print("Type your first number: ")
-local number1 = tonumber(io.read())
-print("Type your second number: ")
-local number2 = tonumber(io.read())
+-- print("Type your first number: ")
+-- local number1 = tonumber(io.read())
+-- print("Type your second number: ")
+-- local number2 = tonumber(io.read())
 
-print("The greatest number is you have typed is: ", math.max(number1, number2))
+-- print("The greatest number is you have typed is: ", math.max(number1, number2))
+
+local str = [[ Olá 
+
+
+          tudo 
+          
+          
+                             bom
+                             
+siuuuu]]
+
+
+print(str)
+
+print("Olá, \\ Mundo!")
+
+print("Maca:\t 1$/kg")
+
+print("Olá, \"abc\" Mundo")
+
+
+local valor = 30
+local nome = "João"
+
+print("O valor é: ", valor)
+print("O valor é:"..valor)
+
+print("O jogador "..nome.."ganhou a ronda!")
+
 
