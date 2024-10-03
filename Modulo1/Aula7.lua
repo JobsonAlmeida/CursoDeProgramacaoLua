b74329c8d1c3917caa24687f02f7f2ba2f0e991d
Jobson Almeida <jobsonbox@gmail.com>
2024-10-03 13:54:48 -0300

    the lesson on IO library has been completed

---
+++ Modulo1/Aula7.lua
@@ -0,0 +1,11 @@
+-- print("Qual o seu nome?")
+-- local thing = io.read()
+-- print("Seu nome é", thing)
+
+print("Type your first number: ")
+local number1 = tonumber(io.read())
+print("Type your second number: ")
+local number2 = tonumber(io.read())
+
+print("The greatest number is you have typed is: ", math.max(number1, number2))
+
