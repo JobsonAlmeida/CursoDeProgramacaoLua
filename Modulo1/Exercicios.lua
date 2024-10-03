1f4d3a77c749620bec3bf8fecc05454119c6844c
Jobson Almeida <jobsonbox@gmail.com>
2024-10-03 16:54:49 -0300

    the second exercise has been made

--- Modulo1/Exercicios.lua
+++ Modulo1/Exercicios.lua
@@ -1,4 +1,15 @@
-print("Choose a number and type it: ")
-local chosenNumber = io.read()
-print("The number chose was: "..chosenNumber)
+print("Type the first grade:")
+local n1 = tonumber(io.read())
 
+print("Type the second grade:")
+local n2 = tonumber(io.read())
+
+print("Type the third grade:")
+local n3 = tonumber(io.read())
+
+print("Type the four grade:")
+local n4 = tonumber(io.read())
+
+local average = (n1+n2+n3+n4)/4
+
+print("The average between "..n1..", "..n2..", "..n3.." and "..n4.." is: "..average)
