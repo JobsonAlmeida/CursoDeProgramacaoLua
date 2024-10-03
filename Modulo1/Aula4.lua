79e1360f8a03ba4ee964a897190a77e1d641db3a
Jobson Almeida <jobsonbox@gmail.com>
2024-10-03 11:19:58 -0300

    the lesson on arithmetic operators has been completed

---
+++ Modulo1/Aula4.lua
@@ -0,0 +1,22 @@
+-- Operadores Aritméticos
+
+print(2 + 10)
+print(10-4)
+print(10*4)
+print(10/4)
+print(10//4)
+print(10%3)
+print(2^3)
+print(-2)
+
+local num1 = 4
+local num2 = 3
+local num3 = 2
+
+print(num1*num2)
+
+local res = num1 + num2
+print(res)
+
+print(num1 + num2 * num3)
+print((num1 + num2) * num3)
