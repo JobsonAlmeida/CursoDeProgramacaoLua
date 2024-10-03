3c677864c242d830cfa8035e5bda8753a7c678c7
Jobson Almeida <jobsonbox@gmail.com>
2024-10-03 10:49:10 -0300

    lesson on Data Types has been completed

---
+++ Modulo1/Aula2.lua
@@ -0,0 +1,17 @@
+local var1 = "Texto"
+local var2 = "Oi"
+
+local sla = "Meu texto"
+
+local number = "25.5"
+local boolean = true
+
+print(type(number)) --aqui eu mostro valores
+
+local var = "Meu texto"
+local num = 30
+local sla = true
+
+print(var, num, sla)
+
+
