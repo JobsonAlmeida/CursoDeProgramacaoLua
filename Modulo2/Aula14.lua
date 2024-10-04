911feefa630f6d9baee623a79ad63977271c1349
Jobson Almeida <jobsonbox@gmail.com>
2024-10-04 11:32:13 -0300

    Completed lesson on compound conditional structure

---
+++ Modulo2/Aula14.lua
@@ -0,0 +1,13 @@
+local num = 5
+local num2 = 10
+
+--Different ways to build an compound conditional structure
+if num > 2 then
+    if num2 > 7 then
+        print("Ambos são maiores")
+    end
+end
+
+if num > 2 and num2 >7 then
+    print("Ambos estão certos")
+end
