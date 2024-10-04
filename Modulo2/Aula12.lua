91771471695b9d92c60ed04922ab7dc061a87f00
Jobson Almeida <jobsonbox@gmail.com>
2024-10-04 10:50:08 -0300

    Completed lesson on if conditional structure

---
+++ Modulo2/Aula12.lua
@@ -0,0 +1,23 @@
+
+-- local num
+
+-- if 5 > 3 then 
+-- print("É verdadeiro")
+-- end
+
+-- local valor = true
+
+-- if valor then
+--     print("Olá Mundo!")
+--     print("O valor é "..tostring(true))
+-- end
+
+-- print("Oi")
+
+print("Digite a sua idade:")
+local idade = tonumber(io.read())
+
+if idade < 18 then
+    print("Você é menor de idade!")
+end
+
--- Modulo2/testes.py
+++ Modulo2/testes.py
@@ -2,3 +2,9 @@ var1 = True
 var2 = False
 
 print(var1 != var2)
+
+if 5 > 3 :
+    print("É verdadeiro")
+
+
+
