257e96e0581ed6dd2621300bb2ffd0a205f421c8
Jobson Almeida <jobsonbox@gmail.com>
2024-10-04 11:22:16 -0300

    Completed lesson on else and elseif conditional structure

---
+++ Modulo2/Aula13.lua
@@ -0,0 +1,25 @@
+
+-- print("Digite a sua idade:")
+-- local idade = tonumber(io.read())
+
+-- if idade >= 18 then
+--     print("Você é maior de idade!")
+-- else
+--     print("Você é menor de idade!")
+-- end
+
+
+print("Digite um número:")
+
+local num = tonumber(io.read())
+
+if num == 1 then
+    print("Olá")
+elseif num == 2 then
+    print("Queijo")
+elseif num ==3 then
+    print("Abacate")
+else
+    print("Nenhuma opção válida")
+end
+
--- Modulo2/testes.py
+++ Modulo2/testes.py
@@ -3,8 +3,17 @@ var2 = False
 
 print(var1 != var2)
 
-if 5 > 3 :
-    print("É verdadeiro")
+numero = int(input("Digite um número: "))
+
+if numero < 5 :
+    print("É totalmente falso")
+elif 5 < numero < 10:
+    print("É parcialmente falso")
+elif  10 <= numero < 20:
+    print("E parcialmente verdadeiro")
+else :
+    print("É totalmente verdadeiro")
+
 
 
 
