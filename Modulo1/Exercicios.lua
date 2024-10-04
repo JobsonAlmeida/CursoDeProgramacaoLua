2da6a72e57dac1048e413dda45ab75bea2262afc
Jobson Almeida <jobsonbox@gmail.com>
2024-10-04 07:59:01 -0300

    the third exercise has been made

--- Modulo1/Exercicios.lua
+++ Modulo1/Exercicios.lua
@@ -1,15 +1,6 @@
-print("Type the first grade:")
-local n1 = tonumber(io.read())
+print("Type the number in meters :")
+local numberMeter = tonumber(io.read())
 
-print("Type the second grade:")
-local n2 = tonumber(io.read())
+local numberCentimeter = numberMeter * 100
 
-print("Type the third grade:")
-local n3 = tonumber(io.read())
-
-print("Type the four grade:")
-local n4 = tonumber(io.read())
-
-local average = (n1+n2+n3+n4)/4
-
-print("The average between "..n1..", "..n2..", "..n3.." and "..n4.." is: "..average)
+print("You have typed "..numberMeter.."m which is equivalente to "..numberCentimeter.."cm")
