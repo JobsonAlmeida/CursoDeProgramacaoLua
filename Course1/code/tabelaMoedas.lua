82c3a070a8b5a1794d7d36dcdffc2ab8dc7cad80
Jobson Almeida <jobsonbox@gmail.com>
2024-10-07 19:10:19 -0300

    lesson 8 not completed. Just watched.

--- Course1/code/tabelaMoedas.lua
+++ Course1/code/tabelaMoedas.lua
@@ -1,53 +1,34 @@
-
-function tostring(res)
+function moedasToString(res)
     local result = ""
     for i, moeda in ipairs(res) do
         result = result.."Nome: "..moeda.nome .. "\n"
-        result = result.."Simbolo"..moeda.simbolo.."\n"
-        result = result.."Pais"..moeda.pais.."\n"
+        result = result.."Simbolo: "..moeda.simbolo.."\n"
+        result = result.."Pais: "..moeda.pais.."\n"
         result = result.."\n"
     end
-
     return result
 end
 
-nomeArquivoMoedas = "arquivo_original.csv"
-arqMoedas= io.open(nomeArquivoMoedas)
-conteudo = arqMoedas:read("a", "r")
 
-posQuebra, fimQuebra = string.find(conteudo, "\n")
-cabecalho = string.sub(conteudo, 1, posQuebra-1)
+function split(texto, sep)
+    local partes = {}
 
+    local posQuebra, fimQuebra = string.find(texto, sep)
+    
+    if posQuebra ~= nil then
+        fimParte = posQuebra -1
+    end
+    local parte = string.sub(texto, 1, fimParte)
+    table.insert(partes, parte)
 
-resultado = {}
+    while posQuebra ~= nil do
+        local iniParte = fimQuebra
+    end
 
-
-while posQuebra ~= nil do
-    local iniLinha = fimQuebra + 1
-    posQuebra, fimQuebra = string.find(conteudo, "\n", iniLinha)
-    local fimLinha = posQuebra - 1
-    linha = string.sub(conteudo, iniLinha, fimLinha)
-
-    local nomeMoeda, simbolo, pais, primeiraLetra = string.match(linha, ";(.-) *;(.-) *;%d-;((%a).-) *;")
-
-    local moeda = 
-    {
-        nome = nomeMoeda,
-        simbolo = simbolo,
-        pais = pais
-    }
-
-    table.insert(resultado, moeda)
 
 end
 
-print(resultado[1])
-str1 = tostring(resultado)
 
-print(str1)
---trabalhando com capturas. As capturas podem ser utilizadas com o %numero
-nomeArquivoResultado = string.gsub(nomeArquivoMoedas, "(%w+)%.%w+$", "_%1_.txt")
-arqResultado = io.open(nomeArquivoResultado, "w")
-arqResultado:write(tostring(resultado))
+res = trim("    agua     ")
 
-
+print(res)
