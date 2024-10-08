9c6028d46db68ccca82f0f4ae1fbaeffae6fb48b
Jobson Almeida <jobsonbox@gmail.com>
2024-10-08 08:42:41 -0300

    completed lesson 9 on I/O

---
+++ Course1/code/ex9.lua
@@ -0,0 +1,20 @@
+
+-- io.write("Digite alguma coisa: ")
+-- a = io.read()
+
+arq = io.open("teste.txt", "r")
+conteudo = arq:read(10)
+print("!"..conteudo.."!")
+
+arq:close()
+
+arqSaida = io.open("testeSaida.txt", "w")
+arqSaida:write("!"..conteudo.."!")
+io.write("Esperando para fechar o arquivo...")
+io.read()
+arqSaida:close()
+io.write("Arquivo fechado. Esperando para sair...")
+io.read()
+
+
+
