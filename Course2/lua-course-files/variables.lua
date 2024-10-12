3987a11dd6632c3f3949ffdc16a67d3a2af573e2
Jobson Almeida <jobsonbox@gmail.com>
2024-10-12 12:07:15 -0300

    Completed lesson on variables

---
+++ Course2/lua-course-files/variables.lua
@@ -0,0 +1,9 @@
+salary1 = 25/2
+
+salary2 = 25//2
+
+print("The value od the salary1 is "..salary1 )
+print("The value od the salary2 is "..salary2 )
+
+name = "Leeroy Jenkins"
+print("The value of name is "..name)
