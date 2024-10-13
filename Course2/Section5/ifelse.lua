41dbd4c3c3a2721252ab33799e1f1a28944fb55d
Jobson Almeida <jobsonbox@gmail.com>
2024-10-13 14:01:39 -0300

    Completed lesson on ifelse

---
+++ Course2/Section5/ifelse.lua
@@ -0,0 +1,13 @@
+menu_option = 3
+
+if menu_option ==1 then
+    menu_text = "Can I play, Daddy?"
+elseif menu_option == 2 then
+    menu_text = "Don't hurt me."
+elseif menu_option == 3 then
+    menu_text = "Bring 'em on!"
+else 
+    menu_text = "I am Death incarnate!"
+end
+
+print("Menu option: "..menu_text)
---
+++ Course2/lua-course-files/quiz.lua
@@ -0,0 +1,6 @@
+
+employee_name = "Jose"
+
+Employee_name = "Maria"
+
+print(employee_name)
